f1ae2b023e01c6c23d1126e65af93fe0f4ae7909
David Frey <dfrey@zipcar.com>
2015-12-15 23:43:37 -0500

    master - Status history returns 8 statuses

--- spec/models/status_message_spec.rb
+++ spec/models/status_message_spec.rb
@@ -8,10 +8,19 @@ describe StatusMessage do
 
     it 'should return a message when one message is added' do
       described_class.create(message: "First status. Everything is A-OK")
+
       expect(described_class.last_ten.count).to eq 1
     end
 
-    it 'should return eight messages when eight messages are added'
+    it 'should return eight messages when eight messages are added' do
+      number_of_messages = 8
+
+      number_of_messages.times do |i|
+        described_class.create(message: "Test ##{i}")
+      end
+
+      expect(described_class.last_ten.count).to eq number_of_messages
+    end
 
     it 'should return the most recent ten messages after eleven messages are added'
   end
