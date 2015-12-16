6bc1e5844f4ec25e8ce2bd6bab4b392d8d3eb600
David Frey <dfrey@zipcar.com>
2015-12-15 23:41:41 -0500

    master - Status history returns a status

--- app/models/status_message.rb
+++ app/models/status_message.rb
@@ -1,6 +1,6 @@
 class StatusMessage < ActiveRecord::Base
   
   def self.last_ten
-    {}
+    StatusMessage.all
   end
 end
--- spec/models/status_message_spec.rb
+++ spec/models/status_message_spec.rb
@@ -5,5 +5,14 @@ describe StatusMessage do
     it 'should return an empty enumerable when there aren\'t any messages' do
       expect(described_class.last_ten).to be_empty
     end
+
+    it 'should return a message when one message is added' do
+      described_class.create(message: "First status. Everything is A-OK")
+      expect(described_class.last_ten.count).to eq 1
+    end
+
+    it 'should return eight messages when eight messages are added'
+
+    it 'should return the most recent ten messages after eleven messages are added'
   end
 end
